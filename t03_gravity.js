3d5836d88fb7c4a8ac22f1618332b97712321235
= <=>
2026-02-09 10:58:28 +1300

    Working on task 3 gravity - adding CanvasToScreen()

--- t03_gravity.js
+++ t03_gravity.js
@@ -1,22 +1,89 @@
 /*******************************************************/
 // P5.play: t03_gravity
 // Sprite falls due to gravity
-// Written by ???
+// Written by Alex Curwen
 /*******************************************************/
-	
+
+/*******************************************************/
+// Notes:
+// middle is [0, 0], top is [0, 1], bottom is [0, -1]
+// position and sizes are relative to canvas HEIGHT
+/*******************************************************/
+
+/*******************************************************/
+// vars
+/*******************************************************/
+
+//Canvas
+var cnv;
+var CNV_WIDTH;
+var CNV_HEIGHT;
+
+//Sprite
+var s_rect;
+var s_rect_x = 0; //x pos
+var s_rect_y = 0.8; //y pos
+const S_RECT_W = 0.1; //width
+const S_RECT_H = 0.1; //height
+
+var s_circ;
+var s_circ_x = 0.1; //x pos
+var s_circ_y = -0.7; //y pos
+const S_CIRC_D = 0.2; //Diameter
+
+var s_platform;
+var s_platform_x = -0.2;
+var s_platform_y = -0.8;
+const S_PLATFORM_W = 0.3; //Width
+const S_PLATFORM_H = 0.05; //Height
+
 /*******************************************************/
 // setup()
+//Initializes the scene and its content
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	//Initialize world
+	world.gravity.y = 9.81;
 
+	//Initialize canvas
+	CNV_WIDTH = windowWidth;
+	CNV_HEIGHT = windowHeight - 100;
+	cnv = new Canvas(CNV_WIDTH, CNV_HEIGHT); //-100 for heading
+
+	//Initialize sprite
+	s_rect = new Sprite((s_rect_x + 1) * CNV_WIDTH / 2, (-s_rect_y + 1) * CNV_HEIGHT / 2, S_RECT_W * CNV_HEIGHT, S_RECT_H * CNV_HEIGHT, 'd');
+	s_rect.color = 'rgb(100, 46, 7)';
+	s_rect.vel.y = 4;
+	s_rect.rotationSpeed = 35.97;
+
+	s_circ = new Sprite((s_circ_x + 1) * CNV_WIDTH / 2, (-s_circ_y + 1) * CNV_HEIGHT / 2, S_CIRC_D * CNV_HEIGHT, 'k');
+	s_circ.color = 'rgb(40, 100, 100)';
+
+	s_platform = new Sprite(s_platform_x,)
 }
-	
+
+class Vec2 {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+/*******************************************************/
+// CanvasToScreen(x,y)
+//Returns [x,y] transformed from canvas space [-1,-1] - [1,1] to world space [-CNV_WIDTH / 2, -CNV_HEIGHT / 2] - [CNV_WIDTH / 2, CNV_HEIGHT / 2]
+/*******************************************************/
+function CanvasToScreen(pos) {
+	return new Vec2((pos.x + 1) * CNV_WIDTH / 2, (-pos.y + 1) * CNV_HEIGHT / 2);
+}	
+
+
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('ccc'); 
 }
 
 /*******************************************************/
